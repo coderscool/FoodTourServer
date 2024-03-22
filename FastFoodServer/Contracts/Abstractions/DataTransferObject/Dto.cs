009e0ecf73e752eef55d16f94ba47d42992f9f84
sasori <trinhthingocngat@gmail.com>
2024-03-22 21:20:56 +0700

    Datatransfer

---
+++ FastFoodServer/Contracts/Abstractions/DataTransferObject/Dto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Abstractions.DataTransferObject
+{
+    public static class Dto
+    {
+        public record Price(string Cost, float Discount);
+        public record Dish(string Description, string Name, string Category, string Image);
+        public record DishItem(Guid Id, Dish Dish, Price Price);
+    }
+}
---
+++ FastFoodServer/Contracts/Services/Identity/Command.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Services.Identity
+{
+    public static class Command
+    {
+        public record RegisterUser(Guid Id, string UserName, string PassWord, string ConfirmPassWord, string Name, string Image);
+    }
+}
